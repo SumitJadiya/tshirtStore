e2cfdd8971d9579a9ed83bf02eafab49e3acbe48
Sumit Jadiya <jadiyaskj@gmail.com>
2021-12-15 08:53:35 +0530

    setup morgan and swagger

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const express = require("express");
-
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 const app = express();
+
+// for swagger documentation
+const swaggerUi = require("swagger-ui-express");
+const yaml = require("yamljs");
+const swaggerDocument = yaml.load("./swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// regular middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// cookie and file middleware
+app.use(cookieParser());
+app.use(fileUpload());
+
+// morgan middleware
+app.use(morgan("tiny"));
+
+// import all routes
+const home = require("./routes/home");
+
+// router middleware
+app.use("/api/v1", home);
 
 // export app js
 module.exports = app;
