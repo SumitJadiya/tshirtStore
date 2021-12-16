a4fc02e668435f9468c81b6d1f6736081796b8fb
Sumit Jadiya <jadiyaskj@gmail.com>
2021-12-16 18:19:21 +0530

    logged in user middleware

---
+++ middlewares/user.js
@@ -0,0 +1,20 @@
+const User = require('../models/user')
+const BigPromise = require('../middlewares/bigPromise')
+const customError = require('../utils/customError')
+const jwt = require('jsonwebtoken')
+
+exports.isLoggedIn = BigPromise(async (req, res, next) => {
+  if (!req.cookies.token || !req.header('Authorization'))
+    return next(new customError(res, 'Please login before accessing this page', 401))
+
+  const token = req.cookies.token || req.header('Authorization').replace('Bearer ', '')
+
+  if (!token)
+    return next(new customError(res, 'Please login before accessing this page', 401))
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+  req.user = await User.findById(decoded.id)
+
+  next()
+})
