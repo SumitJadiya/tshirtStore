924ecc3dbfec167db1d9c0f2280c47652131fa00
Sumit Jadiya <jadiyaskj@gmail.com>
2021-12-23 22:45:44 +0530

    wrap up

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -102,6 +102,17 @@ exports.adminUpdateOrder = BigPromise(async (req, res, next) => {
   })
 })
 
+exports.adminDeleteOrder = BigPromise(async (req, res, next) => {
+  const order = await Order.findById(req.params.id)
+
+  await order.remove()
+
+  res.status(200).json({
+    success: true,
+    message: 'order deleted',
+  })
+})
+
 async function updateProductStock(productId, quantity) {
   const product = await Product.findById(productId)
 
--- routes/order.js
+++ routes/order.js
@@ -5,14 +5,20 @@ const {
   getAllOrdersForUser,
   getAllOrders,
   adminUpdateOrder,
+  adminDeleteOrder,
 } = require('../controllers/orderController')
+
 const router = express.Router()
 const { isLoggedIn, customRole } = require('../middlewares/user')
 
 router.route('/order/create').post(isLoggedIn, createOrder)
 router.route('/order/:id').get(isLoggedIn, getOrderById)
 router.route('/orders').get(isLoggedIn, getAllOrdersForUser)
+
 router.route('/admin/orders').get(isLoggedIn, customRole('admin'), getAllOrders)
-router.route('/admin/order/:id').get(isLoggedIn, customRole('admin'), adminUpdateOrder)
+router
+  .route('/admin/order/:id')
+  .put(isLoggedIn, customRole('admin'), adminUpdateOrder)
+  .delete(isLoggedIn, customRole('admin'), adminDeleteOrder)
 
 module.exports = router
